b04d5b93e7d306e3568c97de12eb7c1f7ed75036
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-26 00:46:36 -0300

    arquivo

---
+++ APPSimplesDeCadastroEm.NET/Enum/Genero.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPSimplesDeCadastroEm.NET
+{
+    public enum Genero
+    {
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 3,
+
+        Documentario = 4,
+
+        Drama = 5,
+
+        Espionagem = 6,
+
+        Faroeste = 7,
+
+        Fantasia = 8,
+
+        Ficacao_Cientifica = 9,
+        
+        Musica = 10,
+
+        Romance = 11,
+
+        Suspense = 12,
+
+        Terror = 13
+
+
+
+
+    }
+}
---
+++ APPSimplesDeCadastroEm.NET/Interfaces/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPSimplesDeCadastroEm.NET.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornarPorId(int id);
+        void Insere(Task entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+
+
+    }
+}
